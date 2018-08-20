030cd992fa05f9109a25be842342debe3c387ff4
Bandoh <kelvinbandoh@hotmail.com>
2018-08-20 23:04:16 +0000

    Starting with Cordinate Geometry

--- test/DartMath_test.dart
+++ test/DartMath_test.dart
@@ -1,9 +1,15 @@
+import 'package:DartMath/CordinateGeometry.dart';
 import 'package:DartMath/DartMath.dart';
+import 'package:DartMath/Fractions.dart';
 import 'package:test/test.dart';
 
 void main() {
  DartMath dartmath = new DartMath();
-
- dartmath.getquotient(8,3);
+ Fractions fractions = new Fractions(7,2);
+ print(fractions.getquotient());
+ print(fractions.getremainder());
+ CordinateGeometry cordinateGeometry = new CordinateGeometry(10,2,6,4);
+ print(cordinateGeometry.getdistance());
+  print(cordinateGeometry.getslope());
  
 }
