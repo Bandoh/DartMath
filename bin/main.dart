b8ea84054e0b1f931f3467fc85364b9375c9ebd1
Bandoh <kelvinbandoh@hotmail.com>
2018-08-21 01:52:18 +0000

    added Lines

--- bin/main.dart
+++ bin/main.dart
@@ -2,11 +2,23 @@ import 'package:DartMath/CordinateGeometry.dart';
 import 'package:DartMath/DartMath.dart' as DartMath;
 
 main(List<String> arguments) {
-  Cordinates p1 = new Cordinates(1.0,1.0);
-  Cordinates p2 = new Cordinates(2.0,3.0);
+  //Intantiate Line Object
+  Line l1 = new Line(1.0, 2.0, 3.0);
+  Line l2 = new Line(1.0, -0.5, 7.0);
+
+//Intanciate Cordinates Object
+  Cordinates p1 = new Cordinates(1.0, 1.0);
+  Cordinates p2 = new Cordinates(2.0, 3.0);
+
   CordinateGeometry cordinateGeometry = new CordinateGeometry();
   print('This is distance ${cordinateGeometry.getdistance(p1, p2)}');
   print('This is Slope ${cordinateGeometry.getslope(p1, p2)}');
   print('This is radius ${cordinateGeometry.getradius(p1, p2)}');
-   print('This is y intercept ${cordinateGeometry.getyintercept(p1, p2)}');
+  print('This is y intercept ${cordinateGeometry.getyintercept(p1, p2)}');
+
+  Cordinates inter;
+  List<double> cord;
+  inter = cordinateGeometry.intersection(l1, l2);
+  cord = inter.getcordinates();
+  print('These Lines intersect at $cord');
 }
--- lib/CordinateGeometry.dart
+++ lib/CordinateGeometry.dart
@@ -1,6 +1,7 @@
 import 'package:DartMath/DartMath.dart';
 import 'dart:math';
 
+//Cordinate Class to handle all cordinates
 class Cordinates {
   double _x;
   double _y;
@@ -8,30 +9,71 @@ class Cordinates {
     this._x = x;
     this._y = y;
   }
+
+  //gets cordinates into an array
+  List<double> getcordinates() {
+    List<double> cordinates = [];
+    cordinates.add(this._x);
+    cordinates.add(this._y);
+    return cordinates;
+  }
+}
+
+//Creating Class for equation of line
+class Line {
+  double _y;
+  double _m;
+  double _x;
+  double _c;
+  Line(double y, double gradient, double intercept, {double x}) {
+    this._y = y;
+    this._m = gradient;
+    this._x = x;
+    this._c = intercept;
+  }
 }
 
 class CordinateGeometry extends DartMath {
   CordinateGeometry();
 
   //Slope of a Straight line
-  double getslope(Cordinates a, Cordinates b) {
-    return ((b._y - a._y) / (b._x - a._x));
+  double getslope(Cordinates point1, Cordinates point2) {
+    return ((point2._y - point1._y) / (point2._x - point1._x));
   }
 
-  //Distance between 2 points
-  double getdistance(Cordinates a, Cordinates b) {
-    double x = (b._x - a._x) * (b._x - a._x);
-    double y = (b._y - a._y) * (b._y - a._y);
+//Returns Cordinates if they intersect else returns null
+  Cordinates intersection(Line line1, Line line2) {
+    try {
+      double top = line2._c - line1._c;
+      double bottom = line2._m - line1._m;
+      double x = top / bottom;
+      double y;
+      y = (line1._m * x) + (line1._c);
+      Cordinates cordinate = new Cordinates(x, y);
+      return cordinate;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  //returns Distance between 2 points
+  double getdistance(Cordinates point1, Cordinates point2) {
+    double x = (point2._x - point1._x) * (point2._x - point1._x);
+    double y = (point2._y - point1._y) * (point2._y - point1._y);
     double sum = x + y;
     return sqrt(sum);
   }
 
-  double getradius(Cordinates a,Cordinates b){
-    return getdistance(a, b);
+
+//Gets radius of 2 cordinates
+  double getradius(Cordinates point1, Cordinates point2) {
+    return getdistance(point1, point2);
   }
 
-  double getyintercept(Cordinates a,Cordinates b){
-    var slope = getslope(a, b);
-    return (a._y)-(slope*a._x);
+
+//Gets Y-intercept from points
+  double getyintercept(Cordinates point1, Cordinates point2) {
+    var slope = getslope(point1, point2);
+    return (point1._y) - (slope * point1._x);
   }
 }
