b85232dfdfc6c0590cdbeb456243017216e851ed
Bandoh <kelvinbandoh@hotmail.com>
2018-08-24 22:16:55 +0000

    trying_to_integrate_Dartdoc

--- lib/dart_math.dart
+++ lib/dart_math.dart
@@ -3,3 +3,7 @@ export 'package:dart_math/src/geometry/fractions.dart';
 export 'package:dart_math/src/geometry/gradient.dart';
 export 'package:dart_math/src/geometry/lines.dart';
 export 'package:dart_math/src/algebra/quadratic.dart';
+
+class DartMath{
+  
+}
